efcb57bf1d520aedbfaa98fe64e25cc809b9f6cb
WillWiggins93 <wwiggins1@una.edu>
2024-11-10 16:34:30 -0600

    Update games.php

--- games.php
+++ games.php
@@ -15,9 +15,45 @@ if (isset($_SESSION['username'])) {
         <title></title>
     </head>
     <body>
-        Display games here...
+        <h3>Games</h3>
         <?php
         // put your code here
+        $servername = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "softball";
+        
+        // create connection
+        $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+        
+        // check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        // retrieve data
+        $sql = "SELECT id, opponent, site, result FROM games";
+        $result = $conn->query($sql);
+        
+        echo "<table border='1'>
+                <tr>
+                <th>ID</th>
+                <th>Opponent</th>
+                <th>Site</th>
+                <th>Result</th>
+                </tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['opponent'] . "</td>";
+            echo "<td>" . $row['site'] . "</td>";
+            echo "<td>" . $row['result'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+
+        $conn->close();
         ?>
     </body>
 </html>
